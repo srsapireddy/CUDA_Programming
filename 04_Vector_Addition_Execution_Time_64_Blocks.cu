1bd9cc51b9cc7ed80baca862f275cc9c00347133
Srinivas Rahul <32967087+srsapireddy@users.noreply.github.com>
2025-06-18 14:54:05 -0500

    Add files via upload

---
+++ 04_Vector_Addition_Execution_Time_64_Blocks.cu
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <cuda.h>
+
+#define SIZE 2048  // Define the size of the vectors
+
+// CUDA Kernel for vector addition
+__global__ void vectorAdd(int* A, int* B, int* C, int n) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+        C[i] = A[i] + B[i];
+}
+
+int main() {
+    int* A, * B, * C;            // Host vectors
+    int* d_A, * d_B, * d_C;      // Device vectors
+    int size = SIZE * sizeof(int);
+
+    // Allocate and initialize host vectors
+    A = (int*)malloc(size);
+    B = (int*)malloc(size);
+    C = (int*)malloc(size);
+
+
+
+    // Allocate device vectors
+    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_B, size);
+    cudaMalloc((void**)&d_C, size);
+
+    for (int i = 0; i < SIZE; i++) {
+        A[i] = i;
+        B[i] = SIZE - i;
+    }
+
+    // Copy host vectors to device
+    cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+
+    // CUDA event creation, used for timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    vectorAdd <<<64, 32 >>> (d_A, d_B, d_C, SIZE);
+    cudaEventRecord(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("Execution time: %f milliseconds\n", milliseconds);
+
+    // Copy result back to host
+    cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+
+    /*
+    // Calculate and print the execution time
+    printf("\nExecution finished\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d + %d = %d  \n", A[i], B[i], C[i]);
+    }
+    */
+
+    // Cleanup
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
+}
